fc4c126d3034322f027d332539c0e4542e0d5d0f
Mkiczek <michaelkiczek@westfieldgrp.com>
2026-01-07 10:47:01 -0500

    Add files via upload
    New commit

---
+++ westfield-ds-portal-no-mdx/src/app/brand/colors/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="container">
+      <h1 className="h1">Color</h1>
+      <p className="p">Primary: #E03100</p>
+<p className="p">Secondary: #4D514D</p>
+<p className="p">Guidance: Primary is reserved for key actions and emphasis. Secondary supports neutral UI and supporting actions.</p>
+<p className="p">Don’t introduce ad-hoc hex colors in product UI.</p>
+    </div>
+  );
+}
