6c8d98a2df6cd6ae3126348a98ed5004edbbdfac
Mkiczek <michaelkiczek@westfieldgrp.com>
2026-01-08 10:28:01 -0500

    Delete westfield-ds-portal-no-mdx directory

--- westfield-ds-portal-no-mdx/src/app/brand/typography/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="container">
-      <h1 className="h1">Typography</h1>
-      <p className="p">Typeface: Roboto</p>
-<p className="p">Use consistent hierarchy (headings, body, labels). Prefer sentence case for labels and headings.</p>
-    </div>
-  );
-}
